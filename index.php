184c85fddc41b75fd73980a7750c9c38a7ee9b14
Al Joslin <allen.joslin@gmail.com>
2022-06-06 14:29:03 -0400

    wip

--- index.php
+++ index.php
@@ -6,51 +6,51 @@
 
  $invalidurl = false;
  if(@simplexml_load_file($url)){
-  $feeds = simplexml_load_file($url);
+	$feeds = simplexml_load_file($url);
  }else{
-  $invalidurl = true;
-  echo "<h2>Invalid RSS feed URL.</h2>";
+	$invalidurl = true;
+	echo "<h2>Invalid RSS feed URL.</h2>";
  }
 
 
  $i=0;
  if(!empty($feeds)){
 
-  $site = $feeds->channel->title;
-  $sitelink = $feeds->channel->link;
+	$site = $feeds->channel->title;
+	$sitelink = $feeds->channel->link;
 
-  echo "<h2>".$site."</h2>";
-  foreach ($feeds->channel->item as $item) {
+	echo "<h2>".$site."</h2>";
+	foreach ($feeds->channel->item as $item) {
 
-    $enclosure = $item->enclosure;
-   echo(print_r($enclosure->url));
+	$enclosure = $item->enclosure;
+	echo(print_r($enclosure["url"]));
 
-   $title = $item->title;
-   $link = $item->enclosure->url;
-   $description = $item->description;
-   $postDate = $item->pubDate;
-   $pubDate = date('D, d M Y',strtotime($postDate));
+	$title = $item->title;
+	$link = $item->enclosure->url;
+	$description = $item->description;
+	$postDate = $item->pubDate;
+	$pubDate = date('D, d M Y',strtotime($postDate));
 
 
-   if($i>=5) break;
-  ?>
-   <div class="post">
-     <div class="post-head">
-       <h2><a class="feed_title" href="<?php echo $link; ?>"><?php echo $title; ?></a></h2>
-       <span><?php echo $pubDate; ?></span>
-     </div>
-     <div class="post-content">
-       <?php echo implode(' ', array_slice(explode(' ', $description), 0, 20)) . "..."; ?> <a href="<?php echo $link; ?>">Read more</a>
-     </div>
-   </div>
+	if($i>=5) break;
+	?>
+	<div class="post">
+		<div class="post-head">
+			<h2><a class="feed_title" href="<?php echo $link; ?>"><?php echo $title; ?></a></h2>
+			<span><?php echo $pubDate; ?></span>
+		</div>
+		<div class="post-content">
+			<?php echo implode(' ', array_slice(explode(' ', $description), 0, 20)) . "..."; ?> <a href="<?php echo $link; ?>">Read more</a>
+		</div>
+	</div>
 
-   <?php
-    $i++;
-   }
+	<?php
+		$i++;
+	}
  }else{
-   if(!$invalidurl){
-     echo "<h2>No item found</h2>";
-   }
+	if(!$invalidurl){
+		echo "<h2>No item found</h2>";
+	}
  }
  ?>
 </div>
