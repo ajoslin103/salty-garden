e8eab0d93de8dcb12a4e638a3bcc43d28b56bfd7
Al Joslin <allen.joslin@gmail.com>
2022-06-06 15:31:17 -0400

    wip

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
 		foreach ($feeds->channel->item as $item) {
 
 			$title = $item->title;
-			$link = (string) $item->enclosure->url;
+			$link = (string) $item->enclosure->attributes()->url;
 			$description = $item->description;
 			$postDate = $item->pubDate;
 			$pubDate = date('D, d M Y',strtotime($postDate));
