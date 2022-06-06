e73d4900b38ebd3dcf4d735b815d59a33aead0f4
Al Joslin <allen.joslin@gmail.com>
2022-06-06 14:14:48 -0400

    revert & minimal change

--- index.php
+++ index.php
@@ -1,48 +1,61 @@
 <div class="content">
 
+ <form method="post" action="">
+  <input type="text" name="feedurl" placeholder="https://salty-garden-64703.herokuapp.com/feed.xml">&nbsp;<input type="submit" value="Submit" name="submit">
+ </form>
  <?php
 
-  $url = "./feed.xml";
+ $url = "https://makitweb.com/feed/";
+ if(isset($_POST['submit'])){
+   if($_POST['feedurl'] != ''){
+     $url = $_POST['feedurl'];
+   }
+ }
 
-  if(@simplexml_load_file($url)){
-    $feeds = simplexml_load_file($url);
-    
-    $i=0;
-    if(!empty($feeds)){
-
-      $site = $feeds->channel->title;
-      $sitelink = $feeds->channel->link;
-
-      echo "<h2>".$site."</h2>";
-      foreach ($feeds->channel->item as $item) {
-
-        echo(var_dump($item));
-        echo("---\n\n");
-
-        $title = $item->title;
-        $link = $item->enclosure->url;
-        $description = $item->description;
-        $postDate = $item->pubDate;
-        $pubDate = date('D, d M Y',strtotime($postDate));
+ $invalidurl = false;
+ if(@simplexml_load_file($url)){
+  $feeds = simplexml_load_file($url);
+ }else{
+  $invalidurl = true;
+  echo "<h2>Invalid RSS feed URL.</h2>";
+ }
 
 
-        if($i>=5) break;
-      ?>
-    
-        <div class="post">
-          <div class="post-head">
-            <h2><a class="feed_title" href="<?php echo $link; ?>"><?php echo $title; ?></a></h2>
-            <span><?php echo $pubDate; ?></span>
-          </div>
-          <div class="post-content">
-            <?php echo implode(' ', array_slice(explode(' ', $description), 0, 20)) . "..."; ?> <a href="<?php echo $link; ?>">Read more</a>
-          </div>
-        </div>
+ $i=0;
+ if(!empty($feeds)){
 
-      <?php
-        $i++;
-      }
-    }
-  }
+  $site = $feeds->channel->title;
+  $sitelink = $feeds->channel->link;
+
+  echo "<h2>".$site."</h2>";
+  foreach ($feeds->channel->item as $item) {
+
+   $title = $item->title;
+   $link = $item->enclosure->url;
+   $description = $item->description;
+   $postDate = $item->pubDate;
+   $pubDate = date('D, d M Y',strtotime($postDate));
+
+
+   if($i>=5) break;
   ?>
-  </div>
+   <div class="post">
+     <div class="post-head">
+       <h2><a class="feed_title" href="<?php echo $link; ?>"><?php echo $title; ?></a></h2>
+       <span><?php echo $pubDate; ?></span>
+     </div>
+     <div class="post-content">
+       <?php echo implode(' ', array_slice(explode(' ', $description), 0, 20)) . "..."; ?> <a href="<?php echo $link; ?>">Read more</a>
+     </div>
+   </div>
+
+   <?php
+    $i++;
+   }
+ }else{
+   if(!$invalidurl){
+     echo "<h2>No item found</h2>";
+   }
+ }
+ ?>
+</div>
