9fa9bfe55bb6ac0a1975eefb8526d6bdc8bf9894
Al Joslin <allen.joslin@gmail.com>
2022-06-06 14:01:52 -0400

    fix php

--- index.php
+++ index.php
@@ -2,54 +2,46 @@
 
  <?php
 
- $url = "https://salty-garden-64703.herokuapp.com/feed.xml";
+  $url = "./feed.xml";
 
- $invalidurl = false;
- if(@simplexml_load_file($url)){
-  $feeds = simplexml_load_file($url);
- }else{
-  $invalidurl = true;
-  echo "<h2>Invalid RSS feed URL.</h2>";
- }
+  if(@simplexml_load_file($url)){
+    $feeds = simplexml_load_file($url);
+    
+    $i=0;
+    if(!empty($feeds)){
 
+      $site = $feeds->channel->title;
+      $sitelink = $feeds->channel->link;
 
- $i=0;
- if(!empty($feeds)){
+      echo "<h2>".$site."</h2>";
+      foreach ($feeds->channel->item as $item) {
 
-  $site = $feeds->channel->title;
-  $sitelink = $feeds->channel->link;
+        echo(var_dump($item));
 
-  echo "<h2>".$site."</h2>";
-  foreach ($feeds->channel->item as $item) {
-
-    echo(var_dump($item))
-
-   $title = $item->title;
-   $link = $item->link;
-   $description = $item->description;
-   $postDate = $item->pubDate;
-   $pubDate = date('D, d M Y',strtotime($postDate));
+        $title = $item->title;
+        $link = $item->link;
+        $description = $item->description;
+        $postDate = $item->pubDate;
+        $pubDate = date('D, d M Y',strtotime($postDate));
 
 
-   if($i>=5) break;
+        if($i>=5) break;
+      ?>
+    
+        <div class="post">
+          <div class="post-head">
+            <h2><a class="feed_title" href="<?php echo $link; ?>"><?php echo $title; ?></a></h2>
+            <span><?php echo $pubDate; ?></span>
+          </div>
+          <div class="post-content">
+            <?php echo implode(' ', array_slice(explode(' ', $description), 0, 20)) . "..."; ?> <a href="<?php echo $link; ?>">Read more</a>
+          </div>
+        </div>
+
+      <?php
+        $i++;
+      }
+    }
+  }
   ?>
-   <div class="post">
-     <div class="post-head">
-       <h2><a class="feed_title" href="<?php echo $link; ?>"><?php echo $title; ?></a></h2>
-       <span><?php echo $pubDate; ?></span>
-     </div>
-     <div class="post-content">
-       <?php echo implode(' ', array_slice(explode(' ', $description), 0, 20)) . "..."; ?> <a href="<?php echo $link; ?>">Read more</a>
-     </div>
-   </div>
-
-   <?php
-    $i++;
-   }
- }else{
-   if(!$invalidurl){
-     echo "<h2>No item found</h2>";
-   }
- }
- ?>
-</div>
+  </div>
